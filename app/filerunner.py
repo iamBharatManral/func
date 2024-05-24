5b488f9e5d58e6824bc463e52fe6ac968e2898d3
iamBharatManral <justbharatmanral@gmail.com>
2024-05-24 21:45:48 +0530

    feat: filerunner - add single statement support

--- app/filerunner.py
+++ app/filerunner.py
@@ -1,6 +1,20 @@
+from app.backend.interpreter import Interpreter
+from app.frontend.lexer.lexer import Lexer
+from app.frontend.parser.parser import Parser
+
+
 def execute_file(filename):
     with open(filename) as f:
         source = f.read()
         if len(source) == 0:
             exit(0)
-        print(source)
+        lexer = Lexer(source)
+        parser = Parser(lexer)
+        pg = parser.parse()
+        if len(parser.errors) > 0:
+            print(parser.errors)
+        else:
+            interpreter = Interpreter()
+            output = interpreter.eval(pg)
+            for out in output:
+                print(out)
